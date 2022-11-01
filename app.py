150bfba60d8fcabce30dc873bf1ebb9b3bfc5005
Ryann <rmahajan4@wisc.edu>
2022-11-01 11:36:16 +0530

    Added API routes (initial)

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask
+import json
+from flask import Flask, Response, request
 from markupsafe import escape
+from transaction import Transaction
 from transaction_tracker import TransactionTracker
 
 app = Flask(__name__)
@@ -9,22 +11,32 @@ tracker = TransactionTracker()
 def hello(name):
     return f"Hello, {escape(name)}!"
 
-def add_transaction(payer, points: int, timestamp):
-    transaction = create_object_for_transactions()
+@app.route('/transaction', methods=['POST'])
+def add_transaction():
+    record = request.json
 
-    if points > 0:
-        tracker.add(transaction)
+    payer, points, timestamp = record['payer'], record['points'], record['timestamp']
+    transaction = Transaction(payer, points, timestamp)
+
+    if int(points) > 0:
+        tracker.track(transaction)
     
     else:
         tracker.reduce_points_for_payer(payer, points)
+    
+    return Response(status=200)
 
-def spend(points: int):
-    while points > 0:
-        transaction = tracker.find_oldest_transaction()
-        tracker.reduce_points_for_payer(transaction.payer, points)
-        payer -= min(transaction.points, points)
+@app.route('/expense', methods = ['POST'])
+def spend():
+    return Response(status=200)
 
+#     while points > 0:
+#         transaction = tracker.find_oldest_transaction()
+#         tracker.reduce_points_for_payer(transaction.payer, points)
+#         payer -= min(transaction.points, points)
+
+@app.route('/balance', methods = ['GET'])
 def get_balance():
-    return ["payer", payer.balance for payer in tracker.payer_list]
+    return {payer: payer.balance for payer in tracker.payers}
 
 
