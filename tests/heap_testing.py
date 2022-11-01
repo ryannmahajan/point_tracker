e363a8cf49129b531328c474095bd183769ccbcc
Ryann <rmahajan4@wisc.edu>
2022-11-01 11:35:52 +0530

    Added heap_testing in separate file

--- tests/heap_testing.py
+++ tests/heap_testing.py
@@ -1,7 +1,22 @@
-from ..transaction import Transaction
+from transaction import Transaction
+from heap import Heap
 
-t = Transaction(payer= "DANNON", points=300, timestamp= "2022-10-31T10:00:00Z")
+def method():
+    t1 = Transaction(payer= "DANNON1", points=300, timestamp= "2022-10-31T10:00:00Z")
+    t3 = Transaction(payer= "DANNON3", points=300, timestamp= "2022-10-31T11:40:00Z")
+    t2 = Transaction(payer= "DANNON2", points=300, timestamp= "2022-10-31T11:00:00Z")
 
-print(t.is_marked_removed())
-print(t.mark_removed())
-print(t.is_marked_removed())
+    h = Heap()
+    h.add(t1)
+    h.add(t3)
+    h.add(t2)
+
+    h.remove(t2)
+    print(h.pop().payer_name)
+    print(h.pop().payer_name)
+    print(h.is_empty())
+
+    print(h.heap)
+    print(type(h))
+
+method()
