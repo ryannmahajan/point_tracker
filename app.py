68423809517885bac15659e343c6babf967775d9
Ryann <rmahajan4@wisc.edu>
2022-11-10 14:25:47 +0530

    Added int(points) and escape for json params

--- app.py
+++ app.py
@@ -10,19 +10,23 @@ tracker = TransactionTracker()
 def add_transaction():
     record = request.json
 
-    payer, points, timestamp = record['payer'], record['points'], record['timestamp']
-    transaction = Transaction(payer, points, timestamp)
-
+    payer, points, timestamp = get_escaped(record['payer'],record['points'], record['timestamp'])
+    transaction = Transaction(payer, int(points), timestamp)
     tracker.track(transaction)
     
     return Response(status=200)
 
 @app.route('/expense', methods = ['POST'])
 def spend():
-    return jsonify(tracker.spend(points = 500))
+    points = escape(request.json['points'])
+    return jsonify(tracker.spend(int(points)))
 
 @app.route('/balance', methods = ['GET'])
 def get_balance():
     return jsonify(tracker.get_balance())
 
+def get_escaped(*argv):
+    return [escape(arg) for arg in argv]
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
