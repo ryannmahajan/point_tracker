1d573868bf1dd6462b5d6fa9fba0a126c211673b
Ryann <rmahajan4@wisc.edu>
2022-11-10 13:37:58 +0530

    Added jsonify calls instead of just dict

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-import json
-from flask import Flask, Response, request
+from flask import Flask, Response, request, jsonify
 from markupsafe import escape
 from transaction import Transaction
 from transaction_tracker import TransactionTracker
 
 app = Flask(__name__)
 tracker = TransactionTracker()
-
-@app.route("/<name>")
-def hello(name):
-    return f"Hello, {escape(name)}!"
 
 @app.route('/transaction', methods=['POST'])
 def add_transaction():
@@ -24,10 +19,10 @@ def add_transaction():
 
 @app.route('/expense', methods = ['POST'])
 def spend():
-    return tracker.spend(points = 500)
+    return jsonify(tracker.spend(points = 500))
 
 @app.route('/balance', methods = ['GET'])
 def get_balance():
-    return tracker.get_balance()
+    return jsonify(tracker.get_balance())
 
 
